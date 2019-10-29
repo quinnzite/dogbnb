e78c34d5bdf9ba11591b7ecb57c8e935243ddbeb
quinn zite <quinnzite@gmail.com>
2019-10-29 12:11:38 +0800

    finished controllers and new user form

--- app/controllers/fosterings_controller.rb
+++ app/controllers/fosterings_controller.rb
@@ -1,2 +1,37 @@
 class FosteringsController < ApplicationController
+  before_action :set_review, only: [:edit, :update, :show, :delete]
+
+  def show
+  end
+
+  def new
+    @fostering = Fostering.new
+  end
+
+  def create
+    @fostering = Fostering.new(review_params)
+    @fostering.user_id = current_user
+    @fostering.dog_id = Dog.find(params[:dog_id])
+    @fostering.save
+    if @fostering.save
+      redirect_to dogs_path
+    else
+      render :new
+    end
+  end
+
+  def delete
+    @fostering.destroy
+    redirect_to dogs_path
+  end
+
+  private
+
+  def set_fostering
+    @fostering = Fostering.find(params[:id])
+  end
+
+  def fostering_params
+    params.require(:fostering).permit([:start_date, :end_date])
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,41 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:edit, :update]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.fostering = Fostering.find(params[:fostering_id])
+    @review.save
+    if @review.save
+      redirect_to dog_path(@review.fostering.dog_id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @review.update(review_params)
+    @review.save
+    if @review.save
+      redirect_to dog_path(@review.fostering.dog_id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit([:rating, :content])
+  end
 end
