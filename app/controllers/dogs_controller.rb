0e72c5a4654135b4008bbd336579fb5bbcd52d4f
quinn zite <quinnzite@gmail.com>
2019-10-30 16:18:25 +0800

    authentication skipping

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,6 @@
 class DogsController < ApplicationController
   before_action :set_dog, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: :index
 
   def index
     @dogs = Dog.all
