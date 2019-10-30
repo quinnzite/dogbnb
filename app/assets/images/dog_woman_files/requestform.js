09bcfd65216ad07074a08afdb108688f3ad47151
Katrina Rochlin <kat.rochlin@gmail.com>
2019-10-30 15:57:04 +0800

    deleting the dog woman

--- app/assets/images/dog_woman_files/requestform.js
+++
@@ -1,6 +0,0 @@
-var s = document.createElement('script');
-s.type = 'text/javascript';
-s.async = true;
-s.src = 'https://ads.themoneytizer.com/s/requestform3.js?siteId=22154&formatId=1';
-var x = document.getElementsByTagName('script')[0];
-x.parentNode.insertBefore(s, x);
