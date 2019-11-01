d800711c6b9c45ac434419742de5ae856cb9c1f3
quinn zite <quinnzite@gmail.com>
2019-11-01 11:13:28 +0800

    css stuff

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,11 +2,10 @@ class Dog < ApplicationRecord
   belongs_to :user
   has_many :fosterings, dependent: :destroy
   has_many :reviews, through: :fosterings
-  validates :name, :breed, :location, presence: true
+  validates :name, :breed, :location, :age, :description, presence: true
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
   mount_uploader :photo, PhotoUploader
-
 
   def unavailable_dates
     fosterings.pluck(:start_date, :end_date).map do |range|
@@ -26,11 +25,8 @@ class Dog < ApplicationRecord
     x = 0
 
     while x < all_fosterings.length
-      # return previous_end_date_next_day if all_fosterings.length == 1
       previous_end_date_next_day = all_fosterings.sort[x][1] + 1
-
       if x + 1 < all_fosterings.length
-
         next_start_date = all_fosterings.sort[x + 1][0]
         if previous_end_date_next_day < next_start_date
           return previous_end_date_next_day
@@ -39,7 +35,6 @@ class Dog < ApplicationRecord
         else
           x += 1
         end
-
       else
         return previous_end_date_next_day
       end
